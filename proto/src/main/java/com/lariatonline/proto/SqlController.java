4a826ff1e4f988fec6c3195137672f332a23dc22
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-01-23 21:00:46 -0500

    checking if eclipse effects yarn

--- proto/src/main/java/com/lariatonline/proto/SqlController.java
+++ proto/src/main/java/com/lariatonline/proto/SqlController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
