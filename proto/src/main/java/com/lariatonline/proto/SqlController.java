be02bb010254ff4021b430e60d0639a8cf23918f
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-01-23 20:24:17 -0500

    .

--- proto/src/main/java/com/lariatonline/proto/SqlController.java
+++ proto/src/main/java/com/lariatonline/proto/SqlController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+//import java.sql.SQLException;
 
 
 @RestController
