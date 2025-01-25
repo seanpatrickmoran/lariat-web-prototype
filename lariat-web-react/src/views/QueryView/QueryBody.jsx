29d63247253adee848d83bbb75c1178bbe149fb4
seanpatrickmoran <seanpatrickmoran@gmail.com>
2025-01-24 21:14:20 -0500

    figured out react changing opt

--- lariat-web-react/src/views/QueryView/QueryBody.jsx
+++ lariat-web-react/src/views/QueryView/QueryBody.jsx
@@ -4,27 +4,74 @@ import React, {useEffect,useState} from 'react';
 // import { fetchTest } from './QueryView.jsx';
 import useLocalStorage from './../CustomHooks/UseLocalStorage.js'
 
-  async function getTableMemory() {
-    // const reply = window.api.invoke('load-table-memory','get table mem');
-    // reply.then((result)=> { return result}); 
-    // return reply
-    console.log('what is it')
-  };
-
 export default class QueryBody extends React.Component{
+         constructor(props){
+            super(props);
+            this.state = {selectValue: ''};  // initial state value
+            // this.ResolutionState = {selectValue: ''};  // initial state value
+            this.resolutionOptions = null;
+            this.offSetQueries = null;
+            this.offset = 0;
+           }
 
-  eventLogger = (e, data) => {
-    console.log('Event: ', e);
-    console.log('Data: ', data);
-  };
+  // componentDidMount(){
+  // };
+
+  handleDatasetChange = (event) => {
+    this.setState({selectValue: event.target.value});
+    console.log(event.target.value);
+    console.log(this.state.selectValue);
+    if (event.target.value != "dataset"){
+      this.resolutionOptions = this.props.storetable[event.target.value].map((el) => <option value={el} key={el}>{el}</option>);
+
+      const fetchPromise = fetch(`http://localhost:8080/api/read_limiter?offset=${this.offset}`);
+      const node = document.getElementById("names");
+
+      fetchPromise.then(response => {
+              return response.json();
+                  }).then(entries => {
+              let names = entries.map(elem => elem.name).join("<option />");
+              node.innerHTML = "<option />" + names;
+              });
+      };
+    };
+
+    handleIncrement = (event) => {
+
+      //check that we have anything new.
+      const fetchPromise = fetch(`http://localhost:8080/api/read_limiter?offset=${this.offset+200}`);
+      const node = document.getElementById("names");
+
+      fetchPromise.then(response => {
+              return response.json();
+                  }).then(entries => {
+              names = [...entries.map(elem => elem.name)];
+              console.log(names);
+              if (names.length!=0){
+                  this.offset += 200;
+                  let names = entries.map(elem => elem.name).join("<option />");
+                  node.innerHTML = "<option />" + names;
+                }
+              });
+    }
 
 
-  componentDidMount(){
-    // const well = await getTableMemory();
+    handleDecrement = (event) => {
+      const fetchPromise = fetch(`http://localhost:8080/api/read_limiter?offset=${this.offset-200}`);
+      const node = document.getElementById("names");
 
-  }
-
-
+      fetchPromise.then(response => {
+              return response.json();
+                  }).then(entries => {
+              names = [...entries.map(elem => elem.name)];
+              console.log(names);
+              if (names.length!=0){
+                  this.offset -= 200;
+                  let names = entries.map(elem => elem.name).join("<option />");
+                  node.innerHTML = "<option />" + names;
+                }
+              });
+    }
 
 
 	  render (){
@@ -47,23 +94,28 @@ export default class QueryBody extends React.Component{
 
     <h1 className="title">SQL Query</h1>
     <div className="row-container">
-      <select name="fields" id="dataset-field-select" selected="dataset">
+      <select name="fields" id="dataset-field-select" selected="dataset" onChange={this.handleDatasetChange}>
         <option value="dataset">Dataset</option>
+        {
+          Object.keys(this.props.storetable).map((key) => {
+              return  <option value={key}>{key}</option>
+              })        
+        }
       </select>
     </div>
 
     <div className="row-container">
       <select name="fields" id="resolution-field-select" selected="resolution">
-        <option value="resolution">Resolution</option>
+        {this.resolutionOptions}
       </select>
     </div>
 
     <div className="row-container">
       <select name="available" id="names-field-select" selected="all">
         <option value="all">All</option>
+        {/*{this.offSetQueries}*/}
       </select>
     </div>
-
     <div className="row-container">
       <button type="button" id="queryBtn">Query</button>
       <button type="button" id="inspectBtn">Inspect</button>
@@ -74,8 +126,8 @@ export default class QueryBody extends React.Component{
     </div>
 
     <div className="row-container offset-navigation" >
-      <button type="button" id="offSetLeftButton">&#8592;</button>
-      <button type="button" id="offSetRightButton">&#8594;</button>
+      <button type="button" id="offSetLeftButton" onClick={this.handleDecrement}>&#8592;</button>
+      <button type="button" id="offSetRightButton" onClick={this.handleIncrement}>&#8594;</button>
     </div>
     <div className="row-container offset-navigation">
       <span id="offsetPage"></span>
--- lariat-web-react/src/views/QueryView/QueryView.jsx
+++ lariat-web-react/src/views/QueryView/QueryView.jsx
@@ -9,15 +9,20 @@ import useLocalStorage from './../CustomHooks/UseLocalStorage.js';
 export default function QueryView() {
 
     const [tableMemory, setTableMemory] = useLocalStorage("tableMemory");
-    console.log("component did mount");
-    console.log(tableMemory);
+    // console.log("component did mount");
+    // console.log(tableMemory);
+
+    // for (var key in tableMemory) {
+    //   console.log(key);
+    //   console.log(tableMemory[key]);
+    // }
+    // tableMemory.keys().forEach((element) => console.log(tableMemory.get(element)));
 
   return (
     <>
       <Head />
-      <QueryBody />
+      <QueryBody storetable={tableMemory}/>
     </>
   )
 }
-
 
